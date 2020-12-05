19701a7af8536c4816d45771b165ddb82f4206d0
mAta <atalaymurat@msn.com>
2020-12-05 16:30:02 +0300

    capistrano

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 const Index = () => {
-  return <div>Hello Next</div>
+  return (
+    <>
+      <h1 style={{background:"beige", width: "100%"}}>Welcome to Index at Next SiteName</h1>
+      <p>We develop nodejs and react apps for web</p>
+    </>
+  )
 }
 
 export default Index
