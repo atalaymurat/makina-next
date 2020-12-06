cbff962bbedd50f7a64604c3d33db6d801eb6b0e
mAta <atalaymurat@msn.com>
2020-12-06 13:41:14 +0300

    meta test

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -4,14 +4,13 @@ import Head from 'next/head'
 const Layout = (props) => (
   <div>
     <Head>
-      <title>makinaTr</title>
+      <title>makinaTr | 2.el Endüstriyel Makina ve Ekipmanlar</title>
+      <meta name="description" content="2.el makina ekipman pazarlama servisleri" />
       <meta property="og:image" content="https://makinatr.com/static/media/siteLogo.a0c44d79.svg" />
       <link rel="stylesheet" href="https://bootswatch.com/4/simplex/bootstrap.min.css" />
     </Head>
     <Navbar />
-    <div className="container">
-    {props.children}
-    </div>
+    <div className="container">{props.children}</div>
   </div>
 )
 
