4675b4572b88bfd738f8c3a49babc3143af31716
mAta <atalaymurat@msn.com>
2020-12-22 03:47:57 +0300

    https

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Layout from '../components/Layout'
 import LeadList from '../components/LeadList'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:3000'
+axios.defaults.baseURL = 'https://localhost:3000'
 
 const Index = (props) => {
 	return (
