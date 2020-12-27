730ebdd71df361ee81b674dc3820d790bd82c5df
mAta <atalaymurat@msn.com>
2020-12-27 16:09:01 +0300

    auto

--- express/index.js
+++ express/index.js
@@ -7,33 +7,54 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-
 app
-  .prepare()
-  .then(() => {
-    const server = express()
-    const apiRoutes = require('./routes')
-    const authRoutes = require('./routes/auth')
+	.prepare()
+	.then(() => {
+		const server = express()
 
-    server.use(bodyParser.json())
-    server.use('/api', apiRoutes)
-    server.use('/api/auth', authRoutes)
+		//Import the mongoose module
+		var mongoose = require('mongoose')
 
-    server.get('*', (req, res) => {
-      return handle(req, res)
-    })
+		//Set up default mongoose connection
+		var mongoDB = 'mongodb://127.0.0.1/makina_nextdb'
+		mongoose.connect(mongoDB, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+			useFindAndModify: false,
+		})
 
-    server.listen(PORT, (err) => {
-      if (err) throw err
-      console.log(`> Running on MODE: [${process.env.NODE_ENV}] Listening on PORT: [${PORT}]`)
+		//Get the default connection
+		var db = mongoose.connection
+		db.once('open', function () {
+			console.log('mongoDb connection status [OK]')
+		})
+
+		//Bind connection to error event (to get notification of connection errors)
+		db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+		/// mongodb end
+		const apiRoutes = require('./routes')
+		const authRoutes = require('./routes/auth')
+
+		server.use(bodyParser.json())
+		server.use('/api', apiRoutes)
+		server.use('/api/auth', authRoutes)
+
+		server.get('*', (req, res) => {
+			return handle(req, res)
+		})
+
+		server.listen(PORT, (err) => {
+			if (err) throw err
+			console.log(`> Running on MODE: [${process.env.NODE_ENV}] Listening on PORT: [${PORT}]`)
 			console.log(`
-					------------------------------
-					secret : ${process.env.SECRET}
-					------------------------------
+ ------------------------------------
+ envProduction : ${process.env.SECRET}
+ ------------------------------------
 				`)
-    })
-  })
-  .catch((err) => {
-    console.error(err.stack)
-    process.exit(1)
-  })
+		})
+	})
+	.catch((err) => {
+		console.error(err.stack)
+		process.exit(1)
+	})
