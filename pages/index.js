d90a47a62fcdb2afc2bd3de0ecbdbe9ad4716da6
mAta <atalaymurat@msn.com>
2020-12-30 02:04:59 +0300

    auto

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ const Index = (props) => {
   return (
     <Layout>
       <div className="h-full bg-gray-900 flex items-start pt-12 flex-col">
-        <h1 className="text-6xl sm:text-7xl lg:text-9xl  px-8 text-white font-bold mx-auto max-w-5xl">
+        <h1 className="text-5xl md:text-7xl lg:text-8xl  px-8 text-white font-bold mx-auto max-w-5xl">
           <span className="text-transparent bg-gradient-to-r bg-clip-text from-blue-500 to-green-500">
             Bir sonraki aşamaya
           </span>
