14411700393c430d5682b83972a2f8d7f9b96354
mAta <atalaymurat@msn.com>
2021-01-03 21:43:52 +0300

    auto

--- components/Layout.js
+++ components/Layout.js
@@ -15,6 +15,7 @@ const Layout = (props) => {
 					{(title && `${title} -- ${process.env.NEXT_PUBLIC_SITE_NAME}`) ||
 						t('site_title', { site_name: process.env.NEXT_PUBLIC_SITE_NAME })}
 				</title>
+				<link rel="canonical" href={process.env.NEXT_PUBLIC_BASE_URL + "/"} />
 				<meta charSet="utf-8" />
 				{noindex && <meta name="robots" content="noindex nofollow" />}
 				<meta
@@ -31,9 +32,9 @@ const Layout = (props) => {
 
 				{/* OPEN GRAPH */}
 				<meta property="og:site_name" content={process.env.NEXT_PUBLIC_SITE_NAME} />
-				<meta property="og:locale" content={Router.locale === "en" ? "en_us" : "tr_tr" } />
-				<meta property="og:type" content={'article'} />
-				<meta property="og:url" content={process.env.NEXT_PUBLIC_BASE_URL + path} />
+				<meta property="og:locale" content={Router.locale === 'en' ? 'en_us' : 'tr_tr'} />
+				<meta property="og:type" content={'website'} />
+				<meta property="og:url" content={`${process.env.NEXT_PUBLIC_BASE_URL}${Router.locale === "en" ? "/en" : ""}${path}`} />
 				<meta
 					property="og:title"
 					content={
@@ -50,24 +51,6 @@ const Layout = (props) => {
 					content={process.env.NEXT_PUBLIC_API_BASE_URL + '/siteLogo.svg'}
 				/>
 
-				{/* TWITTER */}
-				<meta property="twitter:card" content="summary_large_image" />
-				<meta property="twitter:url" content={process.env.NEXT_PUBLIC_BASE_URL + path} />
-				<meta
-					property="twitter:title"
-					content={
-						(title && `${title} -- ${process.env.NEXT_PUBLIC_SITE_NAME}`) ||
-						t('site_title', { site_name: process.env.NEXT_PUBLIC_SITE_NAME })
-					}
-				/>
-				<meta
-					property="twitter:description"
-					content={t('site_description', { site_name: process.env.NEXT_PUBLIC_SITE_NAME })}
-				/>
-				<meta
-					property="twitter:image"
-					content={process.env.NEXT_PUBLIC_API_BASE_URL + '/siteLogo.svg'}
-				/>
 			</Head>
 			<header>
 				<Navbar />
