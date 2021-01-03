7fb162bf2b45eaee6e9764711b0fc16b2d6ce3e9
mAta <atalaymurat@msn.com>
2021-01-04 00:35:24 +0300

    auto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,35 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  purge: {
-    content: process.env.NODE_ENV !== 'development' && [
-      './components/**/*.js',
-      './pages/**/*.js',
-    ],
-  },
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      minHeight: {
-        '(screen-24)': 'calc(100vh - 6rem)',
-      },
-      colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        gray: colors.coolGray,
-        purple: colors.violet,
-        red: colors.red,
-        lime: colors.lime,
-        yellow: colors.amber,
-        green: {
-          750: '#617f41',
-        },
-      },
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/forms')],
+	purge: {
+		content: process.env.NODE_ENV !== 'development' && [
+			'./components/**/*.js',
+			'./pages/**/*.js',
+			'./lib/**/*.js',
+		],
+	},
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			minHeight: {
+				'(screen-24)': 'calc(100vh - 6rem)',
+			},
+			colors: {
+				transparent: 'transparent',
+				current: 'currentColor',
+				gray: colors.coolGray,
+				purple: colors.violet,
+				red: colors.red,
+				lime: colors.lime,
+				yellow: colors.amber,
+				green: {
+					750: '#617f41',
+				},
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [require('@tailwindcss/forms')],
 }
